25b424332c8f872a62f888741a1826f170550cc5
helloplhm-qwq <helloplhm-qwq@outlook.com>
2023-10-14 22:30:00 +0800

    no content

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ import pybind11
 module = Extension(
     'qdes',
     sources=['main.cpp'],
-    include_dirs=[pybind11.get_include(), pybind11.get_include(user=True)]
+    include_dirs=[pybind11.get_include(), pybind11.get_include(user=True)],
+    extra_compile_args=["-std=c++11"],  # 添加编译器选项
 )
 
 setup(
